b19887449b4106678446a5b05927412cba328479
XThinh0804 <thinha4k54cma@gmail.com>
2025-01-24 22:41:26 +0700

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,162 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from "./redux/counter/counterSlice";
-import styles from './styles/Counter.module.css';
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import BookPage from './pages/book';
+import ContactPage from './pages/contact';
+import LoginPage from './pages/login';
+import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import RegisterPage from './pages/register';
+import { callFetchAccount } from './services/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { doGetAccountAction } from './redux/account/accountSlice';
+import Loading from './components/Loading';
+import NotFound from './components/NotFound';
+import AdminPage from './pages/admin';
+import ProtectedRoute from './components/ProtectedRoute';
+import LayoutAdmin from './components/Admin/LayoutAdmin';
+import './styles/reset.scss';
+import './styles/global.scss';
+import ManageUserPage from './pages/admin/user';
+import ManageBookPage from './pages/admin/book';
+import OrderPage from './pages/order';
+import HistoryPage from './pages/history';
+import AdminOrderPage from './pages/admin/order';
 
-export default function App() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
-  const incrementValue = Number(incrementAmount) || 0;
+const Layout = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+    <div className='layout-app'>
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Outlet context={[searchTerm, setSearchTerm]} />
+      <Footer />
     </div>
-  );
+  )
+}
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.account.isLoading)
+
+  const getAccount = async () => {
+    if (
+      window.location.pathname === '/login'
+      || window.location.pathname === '/register'
+    )
+      return;
+
+    const res = await callFetchAccount();
+    if (res && res.data) {
+      dispatch(doGetAccountAction(res.data))
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book/:slug",
+          element: <BookPage />,
+        },
+        {
+          path: "order",
+          element:
+            <ProtectedRoute>
+              <OrderPage />
+            </ProtectedRoute>
+          ,
+        },
+        {
+          path: "history",
+          element:
+            <ProtectedRoute>
+              <HistoryPage />
+            </ProtectedRoute>
+          ,
+        },
+      ],
+    },
+
+    {
+      path: "/admin",
+      element: <LayoutAdmin />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true, element:
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "user",
+          element:
+            <ProtectedRoute>
+              <ManageUserPage />
+            </ProtectedRoute>
+          ,
+        },
+        {
+          path: "book",
+          element:
+            <ProtectedRoute>
+              <ManageBookPage />
+            </ProtectedRoute>
+          ,
+        },
+        {
+          path: "order",
+          element:
+            <ProtectedRoute>
+              <AdminOrderPage />
+            </ProtectedRoute>
+          ,
+        },
+      ],
+    },
+
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
+
+  return (
+    <>
+      {
+        isLoading === false
+          || window.location.pathname === '/login'
+          || window.location.pathname === '/register'
+          || window.location.pathname === '/'
+          || window.location.pathname.startsWith('/book')
+          ?
+          <RouterProvider router={router} />
+          :
+          <Loading />
+      }
+    </>
+  )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../redux/counter/counterSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import accountReducer from '../redux/account/accountSlice';
+import orderReducer from '../redux/order/orderSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['account'] // account will not be persisted
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  order: orderReducer
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+let persistor = persistStore(store);
+
+export { store, persistor };
